28b03e9b10dbbdf4ade11f4dde336b81cf20b869
nishant1194 <144454186+nishant1194@users.noreply.github.com>
2025-04-12 17:21:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import './App.css';
 import Navbar from './components/icl/Navbar';
 import Footer from './components/icl/footer/Footer';
